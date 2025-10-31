5e83b73692d605968906d7d397692258311d2425
nikkurthi <nikkurthi@chssoftwares.com>
2025-10-31 07:34:51 +0530

    project created

---
+++ src/main/java/com/performx/PerformxApiApplication.java
@@ -0,0 +1,13 @@
+package com.performx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformxApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/performx/PerformxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.performx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
